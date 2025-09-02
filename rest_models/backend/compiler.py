937c83b930e25ada2521a08f7ecd0a02f7f2fadf
nicolasromanv <nicolas.romanv@usm.cl>
2025-09-02 16:21:51 -0400

    Revert "UPD: compiler"
    This reverts commit 680eb7336771ac8ad95b4c8620bb5db4fe8a2bf9.

--- rest_models/backend/compiler.py
+++ rest_models/backend/compiler.py
@@ -15,7 +15,7 @@ from django.db.models.base import ModelBase
 from django.db.models.expressions import Col, RawSQL
 from django.db.models.fields.related_lookups import RelatedExact, RelatedIn
 from django.db.models.lookups import Exact, In, IsNull, Lookup, Range
-from django.core.exceptions import EmptyResultSet
+from django.db.models.query import EmptyResultSet
 from django.db.models.sql.compiler import SQLCompiler as BaseSQLCompiler
 from django.db.models.sql.constants import CURSOR, MULTI, NO_RESULTS, ORDER_DIR, SINGLE
 from django.db.models.sql.datastructures import BaseTable
